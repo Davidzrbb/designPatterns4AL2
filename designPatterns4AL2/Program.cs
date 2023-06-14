590c6935b708540c62a7cbab06c0e3b523bf0837
David <zdavid1@myges.fr>
2023-06-14 18:54:03 +0200

    ok

--- designPatterns4AL2/Program.cs
+++ designPatterns4AL2/Program.cs
@@ -13,13 +13,12 @@ public static class MainClass
             "Veuillez choisir votre pizza en précisant le nombre. Exemple : 3 Vegetarienne, 2 Regina, 2 4 Saisons\n");
 
         DisplayMenu();
-        TakeCommand();
-        /*while (true)
+
+        while (true)
         {
             // on boucle pour que le programme n'est pas de fin 
-            //TakeCommand();
-
-        }*/
+            TakeCommand();
+        }
     }
 
     private static void DisplayMenu()
@@ -85,50 +84,24 @@ public static class MainClass
 
 
         CreateFacture(pizzaCommand);
-        /*
-    DisplayRecette(pizzaCommandsList);*/
+        DisplayRecette(pizzaCommand);
     }
 
     private static void DisplayFactureSection(int pizzaCount, Pizza pizza)
     {
         var pizzaName = pizza.Nom;
-        var maxLengthPizza = 12;
+        //get the lenght of the longest pizza.Ingredients name
+        var maxIngredientLength = pizza.Ingredients.Max(s => s.Name.Length);
         var text = pizzaCount + " " + pizzaName + " : " + pizzaCount + " * " + pizza.Prix + "€";
         Total += pizzaCount * pizza.Prix;
         foreach (var ingredient in pizza.Ingredients)
         {
-            text += "\n" + ingredient.Name.PadRight(maxLengthPizza) + " " + ingredient.Quantite * pizzaCount + " " + ingredient.Mesure;
+            text += "\n" + ingredient.Name.PadRight(maxIngredientLength) + " " +
+                    ingredient.Quantite * pizzaCount + " " + ingredient.Mesure;
         }
+
         Console.WriteLine("_________________________");
         Console.WriteLine(text);
-        
-        /*private static void DisplayFacture(List<PizzaCommand> pizzaCommands) {
-            Console.WriteLine("\nFacture : ");
-            var total = 0.0;
-        
-            foreach (var pizzaCmd in pizzaCommands) {
-                var pizzaName = pizzaCmd.Name.Trim();
-                var pizzaCount = pizzaCmd.Count;
-                var maxLengthPizza = pizzaCommands.Max(s => s.Name.Length);
-                foreach (Pizza pizza in Enum.GetValues(typeof(Pizza))) {
-                    var fieldInfo = typeof(Pizza).GetField(pizza.ToString());
-                    var attribut = (PizzaAttribut)Attribute.GetCustomAttribute(fieldInfo, typeof(PizzaAttribut));
-                
-                    if (attribut.Nom.Equals(pizzaName)) {
-                        Console.WriteLine("_________________________");
-                        Console.WriteLine(pizzaCount + " " + pizzaName.PadRight(maxLengthPizza) + " : " + pizzaCount + " * " + attribut.Prix + "€");
-                        total += pizzaCount * attribut.Prix;   
-                        var maxLengthIngredient = attribut.Ingredients.Max(s => s.Nom.Length);
-                        foreach (var ingredient in attribut.Ingredients) {
-                            var price = ingredient.Quantite != 0 ? (ingredient.Quantite * pizzaCount).ToString() : "";
-                            Console.WriteLine(ingredient.Nom.PadRight(maxLengthIngredient) + " " + price +
-                                              " " + ingredient.Unite);
-                        }
-                    }
-                }
-            }
-            Console.WriteLine("Prix total : " + total + "€");
-        }*/
     }
 
     private static void CreateFacture(PizzaCommand pizzaCommand)
@@ -153,35 +126,39 @@ public static class MainClass
 
         Console.WriteLine("Prix total : " + Total + "€");
     }
-}
 
-// Méthode pour afficher la recette
-/*public static void DisplayRecette(List<PizzaCommand> pizzaCommands)
-{
-    foreach (var pizzaCommand in pizzaCommands)
+    private static void DisplayRecette(PizzaCommand pizzaCommand)
     {
-        var pizzaName = pizzaCommand.Name.Trim();
-        var pizzaCount = pizzaCommand.Count;
-        foreach (Pizza pizza in Enum.GetValues(typeof(Pizza)))
+        Console.WriteLine("_________________________");
+        if (pizzaCommand.Regina > 0)
         {
-            var fieldInfo = typeof(Pizza).GetField(pizza.ToString());
-            var attribut = (PizzaAttribut)Attribute.GetCustomAttribute(fieldInfo, typeof(PizzaAttribut));
-            if (attribut.Nom.Equals(pizzaName))
-            {
-                Console.WriteLine("\nRecette de la pizza " + pizzaName + " : \n");
-                var pate = (pizzaName == "Régina") ? "pâte épaisse" : "pâte fine";
-                Console.WriteLine("Déroulez une " + pate);
-                Console.WriteLine("Ajoutez " + attribut.Ingredients[0].Quantite + attribut.Ingredients[0].Unite +
-                                  " de sauce " + attribut.Ingredients[0].Nom);
-                for (int i = 1; i < attribut.Ingredients.Count; i++)
-                {
-                    Console.WriteLine("Ajoutez " + attribut.Ingredients[i].Quantite + " " +
-                                      attribut.Ingredients[i].Unite + " de " + attribut.Ingredients[i].Nom);
-                }
+            DisplayRcetteSection(Pizza.Create("regina"));
+        }
 
-                Console.WriteLine("Enfournez pendant 15 minutes à 210°C");
-            }
+        if (pizzaCommand.QuatreSaisons > 0)
+        {
+            DisplayRcetteSection(Pizza.Create("4saisons"));
+        }
+
+        if (pizzaCommand.Vegetarienne > 0)
+        {
+            DisplayRcetteSection(Pizza.Create("vegetarienne"));
         }
     }
+
+    private static void DisplayRcetteSection(Pizza pizza)
+    {
+        var pizzaName = pizza.Nom;
+
+        Console.WriteLine("\nRecette de la pizza " + pizzaName + " : \n");
+        Console.WriteLine("Préparer la pâte");
+        foreach (var ingredient in pizza.Ingredients)
+        {
+            Console.WriteLine(
+                "Ajouter " + ingredient.Name + " " + ingredient.Quantite + " " + ingredient.Mesure
+            );
+        }
+
+        Console.WriteLine("Cuire la pizza");
+    }
 }
-*/
